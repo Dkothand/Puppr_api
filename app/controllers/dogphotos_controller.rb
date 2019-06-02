4e917bcb973885089238bae3a9540a229befac22
Deva <dkothand@gmail.com>
2019-06-02 12:12:41 -0400

    create route for dog photos, nested in dog routes so only photos can be created when connected to a dog

--- app/controllers/dogphotos_controller.rb
+++ app/controllers/dogphotos_controller.rb
@@ -6,4 +6,23 @@ class DogphotosController < ApplicationController
         render json: @dogphotos
     end
 
+    #POST /dogs/:dog_id/dogphotos
+    def create
+        @dogphoto = DogPhoto.new(dogphoto_params)
+        @dogphoto.dog_id = params[:dog_id]
+
+        if @dogphoto.save
+            render json: @dogphoto, status: :created
+        else
+            render json: @dogphoto.error, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+    # Trusted parameter "whitelist"
+    def dogphoto_params
+        params.require(:dogphoto).permit(:img_link, :details)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :dogphotos
+  resources :dogs do
+    resources :dogphotos, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
