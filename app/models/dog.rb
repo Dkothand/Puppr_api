7e7db4866a2b5f85d7e65850aafd30e948a263cd
Deva <dkothand@gmail.com>
2019-06-02 11:15:19 -0400

    comments

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,6 @@
 class Dog < ApplicationRecord
+    # requires fields have data when creating new dog
     validates :name, :breed, :zip_code, presence: true
+    # one dog has many dog_photos
     has_many :dog_photos
 end
