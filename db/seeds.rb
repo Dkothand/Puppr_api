99369b09f6de9fff2382bcfee981d991067fbed6
Deva <dkothand@gmail.com>
2019-06-03 09:51:57 -0400

    added seed data for dog photos, db:reset to drop and reseed tables with photos having cooresponding dog id

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,12 @@ Dog.create([
     {name: Faker::TvShows::BojackHorseman.character, breed: "Sharpei", temperament: "Shy", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"},
     {name: Faker::TvShows::BojackHorseman.character, breed: "Dachshund", temperament: "Adaptable", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"}
 ])
+
+DogPhoto.create([
+    {img_link: "https://images.unsplash.com/photo-1507146426996-ef05306b995a", details: "Felt cute, might delete later idk", dog_id: 1},
+    {img_link: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9", details: "Selfie of the century", dog_id: 2},
+    {img_link: "https://images.unsplash.com/photo-1494870363241-b5225be3dada", details: "Selfie of the century", dog_id: 3},
+    {img_link: "https://images.unsplash.com/photo-1546659935-24325ae0493d", details: "Look at my bandana!", dog_id: 3},
+    {img_link: "https://images.unsplash.com/photo-1529947320894-e522e075160f", details: "Woke up like this, #nofilter, #beyonce", dog_id: 3},
+    {img_link: "https://images.unsplash.com/photo-1505628346881-b72b27e84530", details: "Selfie of the century", dog_id: 4}
+])
