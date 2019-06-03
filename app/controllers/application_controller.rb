51f80b0c51b939e28af51abd7848bfc63484da51
Deva <dkothand@gmail.com>
2019-06-03 14:30:58 -0400

    see currently logged in users information server-side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,10 @@ class ApplicationController < ActionController::API
         render json: { status: 401, message: 'Unauthorized' }                          
     end 
 
+    # Return current user's information from database
+    def get_current_user
+        return if !bearer_token
+        decoded_jwt = decode_token(bearer_token)
+        User.find(decoded_jwt[0]["user"]["id"])
+    end
 end
