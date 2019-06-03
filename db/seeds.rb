7d222a1f5427f7047a375f94edc9f8bf1c41f938
Deva <dkothand@gmail.com>
2019-06-03 16:43:12 -0400

    added seed users, gave seed dogs user_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User.create(username: '', password: '')
+User.create(username: 'User1', password: 'test')
+User.create(username: 'User2', password: 'test')
+User.create(username: 'User3', password: 'test')
+User.create(username: 'User4', password: 'test')
+User.create(username: 'User5', password: 'test')
 
 Dog.create([
-    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Confident", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005"},
-    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Independent", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10001"},
-    {name: Faker::TvShows::BojackHorseman.character, breed: "German Shepard", temperament: "Laid Back", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005"},
-    {name: Faker::TvShows::BojackHorseman.character, breed: "Sharpei", temperament: "Shy", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"},
-    {name: Faker::TvShows::BojackHorseman.character, breed: "Dachshund", temperament: "Adaptable", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"}
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Confident", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005", user_id: 1},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Independent", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10001", user_id: 2},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "German Shepard", temperament: "Laid Back", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005", user_id: 3},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Sharpei", temperament: "Shy", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027", user_id: 4},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Dachshund", temperament: "Adaptable", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027", user_id: 5}
 ])
 
 DogPhoto.create([
@@ -23,4 +28,5 @@ DogPhoto.create([
     {img_link: "https://images.unsplash.com/photo-1546659935-24325ae0493d", details: "Look at my bandana!", dog_id: 3},
     {img_link: "https://images.unsplash.com/photo-1529947320894-e522e075160f", details: "Woke up like this, #nofilter, #beyonce", dog_id: 3},
     {img_link: "https://images.unsplash.com/photo-1505628346881-b72b27e84530", details: "Selfie of the century", dog_id: 4}
+    {img_link: "https://images.unsplash.com/photo-1550165298-39a6ab9a0a6f", details: "It was such a beautiful day!", dog_id: 5}
 ])
