d9bebbc4f5b270af2425084a23a7c1c1413400e2
Deva <dkothand@gmail.com>
2019-06-03 09:37:31 -0400

    created seed data using Faker gem for Dog model db:reset to drop and reseed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create([
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Confident", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005"},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Labrador Retriever", temperament: "Independent", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10001"},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "German Shepard", temperament: "Laid Back", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10005"},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Sharpei", temperament: "Shy", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"},
+    {name: Faker::TvShows::BojackHorseman.character, breed: "Dachshund", temperament: "Adaptable", bio: Faker::TvShows::BojackHorseman.quote, zip_code: "10027"}
+])
