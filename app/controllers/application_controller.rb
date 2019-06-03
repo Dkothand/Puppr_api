f38ed23fbb187ab0492687e60befa744fc363957
Deva <dkothand@gmail.com>
2019-06-03 14:52:15 -0400

    added comments to functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::API
 
+    # Authenticates user on user routes, must be logged in to access user routes
     def authenticate_token
         puts "AUTHENTICATE JWT"
         render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
     end
 
+    # Removes 'Bearer' and returns just token hash
     def bearer_token                          
         header = request.env['HTTP_AUTHORIZATION']
         pattern = /^Bearer /
         header.gsub(pattern, '') if header && header.match(pattern)                    
     end
     
+    # Decodes token hash
     def decode_token(token_input)
         JWT.decode(token_input, ENV['JWT_SECRET'], true)  
     rescue                       
@@ -24,6 +27,7 @@ class ApplicationController < ActionController::API
         User.find(decoded_jwt[0]["user"]["id"])
     end
 
+    # Checks currently logged in user's id to user_id in token of last login, keeps user from accessing other user's routes
     def authorize_user
         puts "AUTHORIZE USER"
         puts "user id: #{get_current_user.id}"
