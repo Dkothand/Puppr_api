839855605a9d35735612796a2d0440238e53ee96
Deva <dkothand@gmail.com>
2019-06-03 18:51:33 -0400

    configured cors to allow everything in

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001, https://puppr-client.herokuapp.com/' # local port for create-react-app and production URL
+    origins '*' # let everything in
 
     resource '*',
       headers: :any,
